ffb2af4ac3637a7e140f90977c1453a32c7485ac
Anselie <pooksansel@gmail.com>
2025-07-21 01:40:59 +0200

    Update items-script.js

--- items-script.js
+++ items-script.js
@@ -32,7 +32,7 @@ const items = [
   { name: 'Fairy Wings', description: 'Rare. Wings taken from a Fairy.', obtained: 'Unobtainable. Could be bought for 12,500 gems', type: 'Wings'},
   { name: 'Rabbit Rocket', description: 'Rare. Fly up in the sky with 2 Rabbits!', obtauned: 'Unobtainable. Easter item.', type: 'Wings'},
   { name: 'DaVinci Wings', description: 'Legendary. These wings make you look like an inventor!', obtained: 'Obtainable from doing the LEGENDARY QUEST.', type: 'Quest'},
-  { name: 'Seraphim Wings', description: 'Legendary. Radiant Angelic Flight.', obtained: 'Unobtainable, were once obtainable from the LEGENDARY QUEST.', type: 'Quest', 'Wings'},
+  { name: 'Seraphim Wings', description: 'Legendary. Radiant Angelic Flight.', obtained: 'Unobtainable, were once obtainable from the LEGENDARY QUEST.', type: 'Quest' 'Wings'},
 ];
 
 // DOM elements
